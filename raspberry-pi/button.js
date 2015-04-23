0206d14671d7a1c03d1fec43f7518719a1175480
Todd Treece <toddtreece@gmail.com>
2015-04-23 22:02:27 +0000

    adds npm package for raspberry pi examples

--- raspberry-pi/button.js
+++ raspberry-pi/button.js
@@ -1,6 +1,14 @@
 var GpioStream = require('gpio-stream'),
     button = GpioStream.readable(17),
-    AIO = require('adafruit-io'),
-    aio = AIO('xxxxxxxxxxx'); // replace xxxxxxxxxxx with your Adafruit IO key
+    AIO = require('adafruit-io');
 
+// replace xxxxxxxxxxx with your Adafruit IO key
+var AIO_KEY = 'xxxxxxxxxxx';
+    
+// aio init
+var aio = AIO(AIO_KEY);
+
+// pipe button presses to the button feed
 button.pipe(aio.feeds('Button'));
+
+console.log('listening for button presses...');
