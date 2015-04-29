f7b586c929f2bad0e1c96e45da1c29630c7e2f5e
Todd Treece <toddtreece@gmail.com>
2015-04-29 19:30:32 -0400

    adds more comments to fona digital out sketch

--- arduino-fona/button/button.ino
+++ arduino-fona/button/button.ino
@@ -22,9 +22,6 @@ void setup() {
   // set button pin as an input
   pinMode(BUTTON, INPUT_PULLUP);
 
-  // wait until Serial is available
-  while (!Serial);
-
   // Serial init
   Serial.begin(115200);
   Serial.println(F("Initializing....(May take a few seconds)"));
--- arduino-fona/digital-out/digital-out.ino
+++ arduino-fona/digital-out/digital-out.ino
@@ -18,9 +18,6 @@ void setup() {
   // set power switch pin as an output
   pinMode(LIGHT, OUTPUT);
 
-  // wait until Serial is available
-  while (!Serial);
-
   // Serial init
   Serial.begin(115200);
   Serial.println(F("Initializing....(May take a few seconds)"));
@@ -41,30 +38,40 @@ void setup() {
 
 void loop() {
 
+  // get current state and convert it to an int
   char* data = getData("Light");
   int state = atoi(data);
 
+  // turn light on or off
   digitalWrite(LIGHT, state == 1 ? HIGH : LOW);
 
-  // wait 1 second
-  delay(1000);
+  // wait 5 seconds
+  delay(5000);
 
 }
 
 char* getData(char *feed) {
 
+  // init return vars
   uint16_t statuscode;
   int16_t length;
   int16_t i = 0;
 
+  // build URL
   char url[200];
   sprintf(url, AIO_URL, feed, AIO_KEY);
 
+  // dump url for debugging
+  Serial.println(url);
+
+  // make request
   if(! fona.HTTP_GET_start(url, &statuscode, (uint16_t *)&length))
     return "";
 
+  // init char array based on length of response body
   char data[length];
 
+  // read data into char array
   while(length > 0) {
 
     if(fona.available()) {
@@ -77,7 +84,15 @@ char* getData(char *feed) {
 
   }
 
+  // finish request
   fona.HTTP_GET_end();
+
+  // terminate char array
+  data[i] = '\0';
+
+  // dump data for debugging
+  Serial.print("Data: ");
+  Serial.println(data);
 
   return data;
 
