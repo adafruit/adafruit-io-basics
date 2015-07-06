5c5a85da3ac29a87845024688a9d28569304eaf1
Todd Treece <toddtreece@gmail.com>
2015-07-06 15:31:58 -0400

    updates digital out example to use new mqtt lib

--- arduino-cc3000/digital-out/digital-out.ino
+++ arduino-cc3000/digital-out/digital-out.ino
@@ -1,138 +1,177 @@
+/***************************************************
+  Adafruit MQTT Library CC3000 Example
+
+  Designed specifically to work with the Adafruit WiFi products:
+  ----> https://www.adafruit.com/products/1469
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  Adafruit IO example additions by Todd Treece.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <Adafruit_SleepyDog.h>
 #include <Adafruit_CC3000.h>
-#include <PubSubClient.h>
 #include <SPI.h>
+#include "utility/debug.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_CC3000.h"
 
-#define ADAFRUIT_CC3000_IRQ   3
-#define ADAFRUIT_CC3000_VBAT  5
+/****************************** Pins ******************************************/
+
+#define LAMP                  8  // power switch tail pin
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+#define ADAFRUIT_CC3000_VBAT  5  // VBAT & CS can be any digital pins.
 #define ADAFRUIT_CC3000_CS    10
-#define LIGHT                 8
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 
-Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS,
-                                         ADAFRUIT_CC3000_IRQ,
-                                         ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIVIDER);
+/************************* WiFi Access Point *********************************/
 
-Adafruit_CC3000_Client cc3000_client = Adafruit_CC3000_Client();
-PubSubClient client("io.adafruit.com", 1883, mqtt_callback, cc3000_client);
+#define WLAN_SSID       "...your SSID..."  // can't be longer than 32 characters!
+#define WLAN_PASS       "...your password..."
+#define WLAN_SECURITY   WLAN_SEC_WPA2  // Can be: WLAN_SEC_UNSEC, WLAN_SEC_WEP,
+                                       //         WLAN_SEC_WPA or WLAN_SEC_WPA2
 
-// replace with your WiFi connection info
-#define WLAN_SSID "test"
-#define WLAN_PASS "secretpass"
-#define WLAN_SECURITY WLAN_SEC_WPA2
+/************************* Adafruit.io Setup *********************************/
 
-// replace xxxxxxxxx with your Adafruit IO key
-#define AIO_KEY "xxxxxxxxx"
-#define MQTT_TOPIC "api/feeds/Light/data/receive.json"
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
+#define AIO_KEY         "...your AIO key..."
 
-// light state
-int state = 0;
+/************ Global State (you don't need to change this!) ******************/
+
+// Setup the main CC3000 class, just like a normal CC3000 sketch.
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT);
+
+// Store the MQTT server, client ID, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+// Set a unique MQTT client ID using the AIO key + the date and time the sketch
+// was compiled (so this should be unique across multiple devices for a user,
+// alternatively you can manually set this to a GUID or other random value).
+const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the CC3000 MQTT class by passing in the CC3000 class and MQTT server and login details.
+Adafruit_MQTT_CC3000 mqtt(&cc3000, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+
+// You don't need to change anything below this line!
+#define halt(s) { Serial.println(F( s )); while(1);  }
+
+// CC3000connect is a helper function that sets up the CC3000 and connects to
+// the WiFi network. See the cc3000helper.cpp tab above for the source!
+boolean CC3000connect(const char* wlan_ssid, const char* wlan_pass, uint8_t wlan_security);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'lamp' for subscribing to changes.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+const char LAMP_FEED[] PROGMEM = AIO_USERNAME "/feeds/lamp";
+Adafruit_MQTT_Subscribe lamp = Adafruit_MQTT_Subscribe(&mqtt, LAMP_FEED);
+
+/*************************** Sketch Code ************************************/
 
 void setup() {
 
   // set power switch tail pin as an output
-  pinMode(LIGHT, OUTPUT);
+  pinMode(LAMP, OUTPUT);
 
-  // Serial init
   Serial.begin(115200);
-  Serial.println(F("Initializing....(May take a few seconds)"));
 
-  // cc3300 init
-  if (! cc3000.begin()) {
-    Serial.println(F("Couldn't begin()! Check your wiring?"));
+  Serial.println(F("Adafruit IO Example:"));
+  Serial.print(F("Free RAM: ")); Serial.println(getFreeRam(), DEC);
+
+  // Initialise the CC3000 module
+  Serial.print(F("\nInit the CC3000..."));
+  if (! cc3000.begin())
+    halt("Failed");
+
+  // attempt wifi connection
+  while (! CC3000connect(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Retrying WiFi"));
     while(1);
   }
+  Serial.println(F("Connected to WiFi!"));
 
-  Serial.print(F("\nAttempting to connect to ")); 
-  Serial.println(WLAN_SSID);
+  // listen for events on the lamp feed
+  mqtt.subscribe(&lamp);
 
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-    Serial.println(F("Failed!"));
-    while(1);
-  }
-
-  Serial.println(F("Connected!"));
-
-  Serial.println(F("Request DHCP"));
-  while (!cc3000.checkDHCP()) {
-    delay(100);
-  }
-
-  // connect to AIO
-  mqtt_connect();
+  // connect to adafruit io
+  connect();
 
 }
 
 void loop() {
 
-  // required for MQTT connection
-  client.loop();
+  Adafruit_MQTT_Subscribe *subscription;
 
-  // reconnect if we lost connection to AIO
-  if(! client.connected()) {
-    Serial.print(F("AIO connection dropped. Attempting reconnect"));
-    mqtt_connect();
+  // Make sure to reset watchdog every loop iteration!
+  Watchdog.reset();
+
+  // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      connect();
   }
 
-  // write the state to the relay
-  digitalWrite(LIGHT, state == 1 ? HIGH : LOW);
+  // this is our 'wait for incoming subscription packets' busy subloop
+  while (subscription = mqtt.readSubscription(1000)) {
 
-}
+    // we only care about the lamp events
+    if (subscription == &lamp) {
 
-void mqtt_connect() {
+      // convert mqtt ascii payload to int
+      char *value = lamp.lastread;
+      Serial.print(F("Received: "));
+      Serial.println(value);
+      int current = atoi(value);
 
-  char client_name[30];
+      // write the current state to the power switch tail
+      digitalWrite(LAMP, current == 1 ? HIGH : LOW);
 
-  // generate new client name
-  sprintf(client_name, "adafruit-cc3000-%ul", micros());
+    }
 
-  // attempt connection
-  if (client.connect(client_name, AIO_KEY, NULL)) {
-    Serial.println(F("Connected to AIO"));
-    client.subscribe(MQTT_TOPIC);
-    client.loop();
-  } else {
-    Serial.println(F("AIO connect failed"));
   }
 
 }
 
-void mqtt_callback (char* topic, byte* payload, unsigned int length) {
+// connect to adafruit io via MQTT
+void connect() {
 
-  // dump topic and payload from subscription
-  Serial.print(F("Received: "));
-  Serial.println(topic);
-  Serial.write(payload, length);
-  Serial.println("");
+  Serial.print(F("Connecting to Adafruit IO... "));
 
-  // make sure we received a message from the correct feed
-  if(strcmp(topic, MQTT_TOPIC) != 0)
-    return;
+  int8_t ret;
 
-  // convert the MQTT payload to int
-  int value = to_int(payload, length);
+  while ((ret = mqtt.connect()) != 0) {
 
-  // make sure we have a 0 or 1
-  if(value != 0 && value != 1)
-    return;
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default:
+        Serial.println(F("Connection failed"));
+        CC3000connect(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
+        break;
+    }
 
-  // save the new state
-  state = value;
+    if(ret >= 0)
+      mqtt.disconnect();
 
-}
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
 
-int to_int(byte* payload, int length) {
-
-  int i;
-  char val[10];
-
-  // convert payload to char
-  for(i = 0; i < length; i++) {
-    val[i] = payload[i];
   }
 
-  val[i] = '\0';
-
-  // convert char to int and return
-  return atoi(val);
+  Serial.println(F("Adafruit IO Connected!"));
 
 }
+
