7849d11fbbf69fce1fdcc72b9dbc52961332ac8f
Todd Treece <toddtreece@gmail.com>
2015-07-10 10:53:15 -0400

    adds fona group temp/humidity sensor example

---
+++ arduino-fona/group-sensor/group-sensor.ino
@@ -0,0 +1,190 @@
+/***************************************************
+  Adafruit MQTT Library FONA Example
+  Designed specifically to work with the Adafruit FONA
+  ----> http://www.adafruit.com/products/1946
+  ----> http://www.adafruit.com/products/1963
+  ----> http://www.adafruit.com/products/2468
+  ----> http://www.adafruit.com/products/2542
+
+  These cellular modules use TTL Serial to communicate, 2 pins are
+  required to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  Adafruit IO example additions by Todd Treece.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <Adafruit_SleepyDog.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_FONA.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_FONA.h"
+#include <Wire.h>
+#include <Adafruit_AM2315.h>
+
+/****************************** Pins ****************************************/
+#define FONA_RX     2
+#define FONA_TX     3
+#define FONA_RST    4
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+
+/*************************** Cellular APN *************************************/
+
+  // Optionally configure a GPRS APN, username, and password.
+  // You might need to do this to access your network's GPRS/data
+  // network.  Contact your provider for the exact APN, username,
+  // and password values.  Username and password are optional and
+  // can be removed, but APN is required.
+#define FONA_APN       ""
+#define FONA_USERNAME  ""
+#define FONA_PASSWORD  ""
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+#define AIO_USERNAME    "...your AIO username (see https://accounts.adafruit.com)..."
+#define AIO_KEY         "...your AIO key..."
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Store the MQTT server, client ID, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+// Set a unique MQTT client ID using the AIO key + the date and time the sketch
+// was compiled (so this should be unique across multiple devices for a user,
+// alternatively you can manually set this to a GUID or other random value).
+const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the FONA MQTT class by passing in the FONA class and MQTT server and login details.
+Adafruit_MQTT_FONA mqtt(&fona, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+
+// You don't need to change anything below this line!
+#define halt(s) { Serial.println(F( s )); while(1);  }
+
+// FONAconnect is a helper function that sets up the FONA and connects to
+// the GPRS network. See the fonahelper.cpp tab above for the source!
+boolean FONAconnect(const __FlashStringHelper *apn, const __FlashStringHelper *username, const __FlashStringHelper *password);
+
+/****************************** Feeds ***************************************/
+
+// Setup a group called 'weather' for publishing changes.
+// Notice MQTT paths for AIO follow the form: <username>/groups/<feedname>/csv
+const char WEATHER_FEED[] PROGMEM = AIO_USERNAME "/g/weather/csv";
+Adafruit_MQTT_Publish weather = Adafruit_MQTT_Publish(&mqtt, WEATHER_FEED);
+
+/*************************** Sketch Code ************************************/
+
+Adafruit_AM2315 am2315;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.println(F("Adafruit IO Example"));
+
+  // Initialise the FONA module
+  if (! FONAconnect(F(FONA_APN), F(FONA_USERNAME), F(FONA_PASSWORD)))
+    halt("Retrying FONA");
+
+  Serial.println(F("Connected to Cellular!"));
+
+  Watchdog.reset();
+  delay(3000);  // wait a few seconds to stabilize connection
+  Watchdog.reset();
+
+  if (! am2315.begin()) {
+    Serial.println(F("AM2315 not found, check wiring & pullups!"));
+    while (1);
+  }
+  // connect to adafruit io
+  connect();
+
+}
+
+void loop() {
+
+  // Make sure to reset watchdog every loop iteration!
+  Watchdog.reset();
+
+  // ping adafruit io a few times to make sure we remain connected
+  if(! mqtt.ping(3)) {
+    // reconnect to adafruit io
+    if(! mqtt.connected())
+      connect();
+  }
+
+  char sendbuffer[80];
+  char numberbuffer[20];
+  float temp = (am2315.readTemperature() * 1.8) + 32;
+  float humidity = am2315.readHumidity();
+
+  // add temp feed name
+  strcpy(sendbuffer, "temp,");
+
+  // add temp value
+  dtostrf(temp, 2, 6, numberbuffer);
+  strcat(sendbuffer, numberbuffer);
+
+  // add new line and humidity feed name
+  strcat(sendbuffer, "\nhumidity,");
+
+  // add humidity value
+  dtostrf(humidity, 2, 6, numberbuffer);
+  strcat(sendbuffer, numberbuffer);
+
+  // Now we can publish stuff!
+  Serial.println(F("\nSending: "));
+  Serial.println(sendbuffer);
+
+  if (! weather.publish(sendbuffer))
+    Serial.println(F("Failed."));
+  else
+    Serial.println(F("Success!"));
+
+  delay(1000);
+
+}
+
+// connect to adafruit io via MQTT
+void connect() {
+
+  Serial.print(F("Connecting to Adafruit IO... "));
+
+  int8_t ret, retries = 5;
+
+  while ((ret = mqtt.connect()) != 0) {
+
+    switch (ret) {
+      case 1: Serial.println(F("Wrong protocol")); break;
+      case 2: Serial.println(F("ID rejected")); break;
+      case 3: Serial.println(F("Server unavail")); break;
+      case 4: Serial.println(F("Bad user/pass")); break;
+      case 5: Serial.println(F("Not authed")); break;
+      case 6: Serial.println(F("Failed to subscribe")); break;
+      default: Serial.println(F("Connection failed")); break;
+    }
+
+    if(ret >= 0)
+      mqtt.disconnect();
+
+    retries--;
+
+    if (retries == 0)
+      halt("Resetting system");
+
+    Serial.println(F("Retrying connection..."));
+    delay(5000);
+
+  }
+
+  Serial.println(F("Adafruit IO Connected!"));
+
+}
