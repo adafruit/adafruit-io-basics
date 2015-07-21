50bbf9a40e572c28ce0a5bc01af5c2cc56af6710
Todd Treece <toddtreece@gmail.com>
2015-07-21 16:56:14 -0400

    adds battery level to esp trigger example

--- esp8266/trigger/trigger.ino
+++ esp8266/trigger/trigger.ino
@@ -14,6 +14,7 @@
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 #include <ESP8266WiFi.h>
+#include <EEPROM.h>
 #include "Adafruit_IO_Client.h"
 
 // door gpio pin
@@ -23,27 +24,36 @@
 #define WLAN_SSID       "...your SSID..."
 #define WLAN_PASS       "...your password..."
 
-// Configure Adafruit IO access.
+// configure Adafruit IO access
 #define AIO_KEY         "...your AIO key..."
 
+// create an Adafruit IO client instance
 WiFiClient client;
-
-// Create an Adafruit IO Client instance.  Notice that this needs to take a
-// WiFiClient object as the first parameter, and as the second parameter a
-// default Adafruit IO key to use when accessing feeds (however each feed can
-// override this default key value if required, see further below).
 Adafruit_IO_Client aio = Adafruit_IO_Client(client, AIO_KEY);
-
-// Finally create instances of Adafruit_IO_Feed objects, one per feed.  Do this
-// by calling the getFeed function on the Adafruit_IO_FONA object and passing
-// it at least the name of the feed, and optionally a specific AIO key to use
-// when accessing the feed (the default is to use the key set on the
-// Adafruit_IO_Client class).
-Adafruit_IO_Feed door = aio.getFeed("door");
 
 void setup() {
 
+  EEPROM.begin(512);
   pinMode(DOOR, INPUT_PULLUP);
+
+  // get the current count position from eeprom
+  byte battery_count = EEPROM.read(0);
+
+  // we only need this to happen once every minute,
+  // so we use eeprom to track the count between resets.
+  if(battery_count >= 30) {
+    // reset counter
+    battery_count = 0;
+    // report battery level to Adafruit IO
+    battery_level();
+  } else {
+    // increment counter
+    battery_count++;
+  }
+
+  // save the current count
+  EEPROM.write(0, battery_count);
+  EEPROM.commit();
 
   // if door isn't open, we don't need to send anything
   if(digitalRead(DOOR) == LOW) {
@@ -51,6 +61,22 @@ void setup() {
     ESP.deepSleep(2000000, WAKE_RF_DISABLED);
     return;
   }
+
+  // the door is open if we have reached here,
+  // so we should send a value to Adafruit IO.
+  door_open();
+
+  // we are done here. go back to sleep.
+  ESP.deepSleep(2000000, WAKE_RF_DISABLED);
+
+}
+
+// noop
+void loop() {}
+
+// connect to wifi network. used by
+// door and battery functions.
+void wifi_init() {
 
   // wifi init
   WiFi.begin(WLAN_SSID, WLAN_PASS);
@@ -62,14 +88,40 @@ void setup() {
   // AIO init
   aio.begin();
 
-  // send value to AIO
-  door.send("1");
+}
 
-  // sleep for a couple seconds
-  ESP.deepSleep(2000000, WAKE_RF_DISABLED);
+void door_open() {
+
+  // turn on wifi if we aren't connected
+  if(WiFi.status() != WL_CONNECTED)
+    wifi_init();
+
+  // grab the door feed
+  Adafruit_IO_Feed door = aio.getFeed("door");
+
+  // send door open value to AIO
+  door.send("1");
 
 }
 
-// noop
-void loop() {}
+void battery_level() {
+
+  // read the battery level from the ESP8266 analog in pin.
+  // analog read level is 10 bit 0-1023 (0V-1V).
+  // our 10K & 2.2K voltage divider takes the max
+  // lipo value of 4.2V and drops it to 0.758V max.
+  // this means our max analog read value should be 774
+  int level = analogRead(A0);
+
+  // turn on wifi if we aren't connected
+  if(WiFi.status() != WL_CONNECTED)
+    wifi_init();
+
+  // grab the battery feed
+  Adafruit_IO_Feed battery = aio.getFeed("battery");
+
+  // send battery level to AIO
+  battery.send(level);
+
+}
 
